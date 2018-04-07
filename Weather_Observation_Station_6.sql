498125122adbb1a7404aeea6acff2c78d9ca8557
Jason <157136443@qq.com>
2018-04-07 10:22:45 +0800

    Basic Select
    Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.

---
+++ Weather_Observation_Station_6.sql
@@ -0,0 +1,5 @@
+select distinct CITY from STATION where CITY like 'A%';
+select distinct CITY from STATION where CITY like 'E%';
+select distinct CITY from STATION where CITY like 'I%';
+select distinct CITY from STATION where CITY like 'O%';
+select distinct CITY from STATION where CITY like 'U%';
